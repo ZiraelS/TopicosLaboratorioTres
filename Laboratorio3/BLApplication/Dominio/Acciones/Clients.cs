b34162ede55eff55532c45a87ba3c6ed72ce5826
laboratorio <laboratorio@LAB9-07>
2018-03-24 16:43:28 -0600

    Commit de WCF
    Inicio de WCF sin Conexion a Base de Datos
    Daniel Lepiz

---
+++ Laboratorio3/BLApplication/Dominio/Acciones/Clients.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BLApplication.Dominio.Acciones
+{
+    public class Clients
+    {
+        public IList<Model.Client> ListarAproximadaNombreClientes(string elNombre)
+        {
+            var elRepositorio = new Dominio.Repositorio.Clients();
+            return elRepositorio.ListarAproximadaNombreClientes(elNombre);
+        }
+
+        public IList<Model.Client> ListarLibrosSolicitadosPorCliente(string elCliente)
+        {
+            var elRepositorio = new Dominio.Repositorio.Clients();
+            return elRepositorio.ListarAproximadaNombreClientes(elCliente);
+        }
+    }
+}
---
+++ Laboratorio3/BLApplication/Dominio/Especificaciones/Clients.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BLApplication.Dominio.Especificaciones
+{
+    public class Clients
+    {
+        public IList<Model.Client> ListarAproximadaNombreClientes(string elNombre)
+        {
+            var laAccion = new Dominio.Acciones.Clients();
+            return laAccion.ListarAproximadaNombreClientes(elNombre);
+        }
+
+        public IList<Model.Client> ListarLibrosSolicitadosPorCliente(string elCliente)
+        {
+            var laAccion = new Dominio.Acciones.Clients();
+            return laAccion.ListarLibrosSolicitadosPorCliente(elCliente);
+        }
+    }
+}
