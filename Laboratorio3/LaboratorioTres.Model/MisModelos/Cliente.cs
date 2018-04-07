0dc05609358cde73fd20209112490ac65a5233d3
laboratorio <laboratorio@LAB9-07>
2018-04-07 17:19:55 -0600

    Creacion de Modelo
    LaboratorioTres.Model
    Primer Propiedad

---
+++ Laboratorio3/LaboratorioTres.Model/MisModelos/Cliente.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaboratorioTres.Model
+{
+
+    public partial class Cliente
+    {
+        [DataMember]
+        [NotMapped]
+        public int EdadCliente
+        {
+            get
+            {
+                DateTime? Nacimiento = FechaNacimiento;
+                DateTime Hoy = DateTime.Today;
+                return (int)((Hoy - (DateTime)Nacimiento).TotalDays / (365.25 / 12));
+            }
+            set { }
+        }
+    }
+}
